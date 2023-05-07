75297d9bf675c515411543bdaaa07ae665b2dd8d
StasDogonov <132846793+StasDogonov@users.noreply.github.com>
2023-05-07 22:42:01 +0300

    Add files via upload

---
+++ Lab3_task1.bat
@@ -0,0 +1,33 @@
+@echo off
+
+mkdir "Lab3\Group\Dogonov\Command_line\Hidden_folder"
+mkdir "Lab3\Group\Dogonov\Command_line\folder"
+
+echo: Folders were created!
+
+tree /a /f
+
+attrib +h "Lab3\Group\Dogonov\Command_line\Hidden_folder"
+echo: The "Hidden_folder" Folder was hidden!
+
+xcopy /? > "Lab3\Group\Dogonov\Command_line\folder\copyhelp.txt"
+echo: "copyhelp.txt" File .txt created!
+
+xcopy > "Lab3\Group\Dogonov\Command_line\folder\copyhelp.txt" "Lab3\Group\Dogonov\Command_line\Hidden_folder\copied_copyhelp.txt"
+echo: "copied_copyhelp.txt" File .txt created!
+
+mkdir "Lab3\Group\Dogonov\Command_line\Hidden_folder"
+mkdir "Lab3\Group\Dogonov\Command_line\folder"
+
+echo: Folders were created!
+
+attrib +h "Lab3\Group\Dogonov\batch\Hidden_folder"
+echo: The "Hidden_folder" folder was hidden!
+
+xcopy /? > "Lab3\Group\Dogonov\batch\folder\copyhelp.txt"
+echo: "copyhelp.txt" File .txt created!
+
+xcopy > "Lab3\Group\Dogonov\batch\folder\copyhelp.txt" "Lab3\Group\Dogonov\batch\Hidden_folder\copied_copyhelp.txt"
+echo: "copied_copyhelp.txt" File .txt created!
+
+tree /a /f
---
+++ Lab3_task2.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+if "%1"=="" (
+    echo Syntax: %~nx0 directory_path
+    echo.
+    echo directory_path
+    exit /b 1
+)
+
+set "dir=%1"
+if not exist "%dir%" (
+    echo Каталог %dir% не існує.
+    exit /b 1
+)
+
+set /a count=0
+for /f "tokens=*" %%i in ('dir /s /a:d /b /a:r /a:h /ad %1') do (
+    set /a count+=1
+)
+
+echo Кількість підкаталогів в каталозі %dir%: %count%
+pause
+exit /b 0

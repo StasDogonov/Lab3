28050df8059709ac1f43a438f4e30b9c70bdedc3
StasDogonov <132846793+StasDogonov@users.noreply.github.com>
2023-05-07 22:50:48 +0300

    Update Lab3_task1.bat

--- Lab3_task1.bat
+++ Lab3_task1.bat
@@ -11,10 +11,10 @@ attrib +h "Lab3\Group\Dogonov\Command_line\Hidden_folder"
 echo: The "Hidden_folder" Folder was hidden!
 
 xcopy /? > "Lab3\Group\Dogonov\Command_line\folder\copyhelp.txt"
-echo: "copyhelp.txt" File .txt created!
+echo: "copyhelp.txt" File copyhelp.txt created!
 
 xcopy > "Lab3\Group\Dogonov\Command_line\folder\copyhelp.txt" "Lab3\Group\Dogonov\Command_line\Hidden_folder\copied_copyhelp.txt"
-echo: "copied_copyhelp.txt" File .txt created!
+echo: "copied_copyhelp.txt" File copied_copyhelp.txt created!
 
 mkdir "Lab3\Group\Dogonov\Command_line\Hidden_folder"
 mkdir "Lab3\Group\Dogonov\Command_line\folder"
@@ -25,9 +25,9 @@ attrib +h "Lab3\Group\Dogonov\batch\Hidden_folder"
 echo: The "Hidden_folder" Folder was hidden!
 
 xcopy /? > "Lab3\Group\Dogonov\batch\folder\copyhelp.txt"
-echo: "copyhelp.txt" File .txt created!
+echo: "copyhelp.txt" File copyhelp.txt created!
 
 xcopy > "Lab3\Group\Dogonov\batch\folder\copyhelp.txt" "Lab3\Group\Dogonov\batch\Hidden_folder\copied_copyhelp.txt"
-echo: "copied_copyhelp.txt" File .txt created!
+echo: "copied_copyhelp.txt" File copied_copyhelp.txt created!
 
 tree /a /f
